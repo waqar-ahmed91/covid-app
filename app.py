d7f406e09d41eba388cca7039e29e22b4788b487
Waqar <waqar.nu@gmail.com>
2023-09-14 02:53:59 +1000

    updating files

--- app.py
+++ app.py
@@ -28,19 +28,19 @@ model.add(MaxPooling2D(pool_size=(2,2)), name="layer2")
 # add another convolutional layer
 model.add(Conv2D(64, (3,3), padding='SAME', activation='relu', name="layer3"))
 # pooling layer
-model.add(MaxPooling2D(pool_size=(2,2)), name="layer3")
+model.add(MaxPooling2D(pool_size=(2,2)), name="layer4")
 # place a dropout layer
 #model.add(Dropout(0.5))
 # add another convolutional layer
-model.add(Conv2D(64, (3,3), padding='SAME', activation='relu'), name="layer4")
+model.add(Conv2D(64, (3,3), padding='SAME', activation='relu'), name="layer5")
 # pooling layer
-model.add(MaxPooling2D(pool_size=(2,2)), name="layer5")
+model.add(MaxPooling2D(pool_size=(2,2)), name="layer6")
 # Flatten layer
 model.add(Flatten())
 # add a dense layer : amount of nodes, activation
-model.add(Dense(512, activation='relu'), name="layer6")
-model.add(Dense(256,activation='relu'), name="layer7")
-model.add(Dense(128,activation='relu'), name="layer8")
+model.add(Dense(512, activation='relu'), name="layer7")
+model.add(Dense(256,activation='relu'), name="layer8")
+model.add(Dense(128,activation='relu'), name="layer9")
 model.add(Dropout(0.5))
 model.add(Dense(n_classes,activation='sigmoid'), name="output_layer")
 model.compile(Adam(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
